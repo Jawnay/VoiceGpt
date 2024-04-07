2f4f690500d7984ddb5bcba8a33078b8824ce4c4
Jonathan Ly <Jly2003@gmail.com>
2024-04-07 02:54:37 -0700

    last hope

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('https://jawnay.github.io', cors(corsOptions)); 
+app.options('*', cors(corsOptions)); 
 
 app.post('/api/text-to-audio-file', async (req, res) => {
 

9472809dc77e9673f57463a7b9c0dc7142175cf1
Jonathan Ly <Jly2003@gmail.com>
2024-04-07 02:10:31 -0700

    god are you there

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/VoiceGPT/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/VoiceGpt/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
